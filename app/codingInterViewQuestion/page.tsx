6ca4151a333b790aea57d0452d76d3362ca85df2
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-10-12 12:38:55 +0530

    add the all page in coding interview question

--- app/codingInterViewQuestion/page.tsx
+++ app/codingInterViewQuestion/page.tsx
@@ -1,11 +1,25 @@
+import { LinkData } from '@/data/LinkData';
+import Link from 'next/link';
 import React from 'react'
 
 const codingInterViewQuestion = () => {
   return (
     <div>
-      Lorem ipsum dolor sit amet.
+      <div className="container m-auto  py-20  flex gap-10 flex-wrap ">
+        {LinkData.map((value) => {
+          return (
+            <Link
+              className="bg-gray-950 text-white px-8 py-3 text-xl hover:bg-gray-800"
+              href={value.url}
+              key={value.id}
+            >
+              {value.name}
+            </Link>
+          );
+        })}
+      </div>{" "}
     </div>
-  )
+  );
 }
 
 export default codingInterViewQuestion
