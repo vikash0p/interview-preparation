55a330a0623fb3aa0e0db574ac97665e76126ddb
Vikash <119168528+vikash0p@users.noreply.github.com>
2025-01-17 10:42:10 +0530

    update React interview question links to use consistent path structure

--- components/ReactInterview/ReactInterViewQuestionLink.ts
+++ components/ReactInterview/ReactInterViewQuestionLink.ts
@@ -3,31 +3,31 @@ export const ReactInterViewQuestionLink = [
   {
     id: "1",
     title: "What is React",
-    link: "/reactInterviewQuestion/what-is-react",
+    link: "/React/what-is-react",
   },
   {
     id: "2",
     title: "Advantages of React",
-    link: "/reactInterviewQuestion/advantages-of-react",
+    link: "/React/advantages-of-react",
   },
   {
     id: "3",
     title: "Limitation for functional component",
-    link: "/reactInterviewQuestion/FunctionalComponentLimitations",
+    link: "/React/FunctionalComponentLimitations",
   },
   {
     id: "4",
     title: "state in react",
-    link: "/reactInterviewQuestion/state-in-react",
+    link: "/React/state-in-react",
   },
   {
     id: "5",
     title:'stateful component in react',
-    link: "/reactInterviewQuestion/stateful-component-in-react",
+    link: "/React/stateful-component-in-react",
   },
   {
     id:'7',
     title:"props in react",
-    link: "/reactInterviewQuestion/props-in-react",
+    link: "/React/props-in-react",
   }
 ];
