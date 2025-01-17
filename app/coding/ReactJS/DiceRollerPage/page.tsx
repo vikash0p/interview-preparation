8d8c620e3a15cdd29b7782b0c41a51c5579d885d
Vikash <119168528+vikash0p@users.noreply.github.com>
2025-01-17 11:23:42 +0530

    refactor: consolidate DiceRoller component and implement Infinite Scroll feature for student list

--- app/coding/ReactJS/DiceRollerPage/page.tsx
+++ app/coding/ReactJS/DiceRollerPage/page.tsx
@@ -1,12 +1,28 @@
-import DiceRoller from '@/components/PracticalQuestion/DiceRoller'
-import React from 'react'
+"use client";
+import React, { useCallback, useState } from "react";
 
-const DiceRollerPage = () => {
+const DiceRoller = () => {
+  const [dice, setDice] = useState(0);
+
+  const rollDice = useCallback(() => {
+    const randomNumber = Math.floor(Math.random() * 6) + 1; // Generates a number between 1 and 6
+    setDice(randomNumber);
+  }, []);
+
   return (
-    <div>
-        <DiceRoller />
+    <div className="flex flex-col items-center mt-5">
+      <button
+        type="button"
+        className="px-8 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 transition-all"
+        onClick={rollDice}
+      >
+        Roll Dice
+      </button>
+      <div className="text-2xl font-bold mt-3">
+        {dice > 0 ? `You rolled: ${dice}` : "Roll the dice!"}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DiceRollerPage
+export default DiceRoller;
