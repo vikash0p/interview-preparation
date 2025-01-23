cc84d2c94dfff95f6f9395ec304831f86a6cf716
Vikash <119168528+vikash0p@users.noreply.github.com>
2025-01-23 10:24:14 +0530

    refactor: remove local font imports and simplify body class in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/GlobalComponents/Navbar";
-
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "InterViewPrap | Ace Your Job Interviews",
@@ -59,9 +46,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-gray-400 bg-black`}
-      >
+      <body className={` antialiased text-gray-400 bg-black`}>
         <Navbar />
         <main>{children}</main>
       </body>
