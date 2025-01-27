79fd67bed8f57333675565928f45b7f0b1adb95c
Vikash <119168528+vikash0p@users.noreply.github.com>
2025-01-27 10:46:36 +0530

    refactor: add ScrollToTop component; implement scroll visibility logic and smooth scroll functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/GlobalComponents/Navbar";
 import { LayoutMetadata } from "@/utils/seo/meta/LayoutMetadata";
+import ScrollToTop from "@/components/GlobalComponents/ScrollToTop";
 
 export const metadata: Metadata = LayoutMetadata;
 
@@ -15,6 +16,7 @@ export default function RootLayout({
       <body className={` antialiased text-gray-400 bg-black`}>
         <Navbar />
         <main>{children}</main>
+        <ScrollToTop />
       </body>
     </html>
   );
