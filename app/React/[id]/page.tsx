a3d2232cefacb11486d89b8bacbb393d2d69c896
vikash0p <vikash2000158@gmail.com>
2025-06-03 11:37:49 +0530

    feat: refactor and reorganize reusable components for improved structure and readability

--- app/React/[id]/page.tsx
+++ app/React/[id]/page.tsx
@@ -1,36 +1,42 @@
-'use client'
-import Code from "@/components/ReusableComponents/Code";
-import { ReactMockData } from "@/main/data/ReactData/ReactMockData";
-import React, { useMemo, useState } from "react";
+'use client';
+import Code from '@/components/reusable-components/Code';
+import { ReactMockData } from '@/main/data/ReactData/ReactMockData';
+import React, { useMemo, useState } from 'react';
 
 const ReactId = ({ params }: { params: { id: string } }) => {
-  console.log("🚀 ~ file: page.tsx:4 ~ params:", params);
+	console.log('🚀 ~ file: page.tsx:4 ~ params:', params);
 
-  const[open,setOpen]=useState(false);
-  const toggleOpen=()=>{
-    setOpen(!open);
-  }
-  const ReactSingleData = useMemo(() => {
-    return ReactMockData.find((value) => String(value.link) === params.id);
-  }, [params.id]);
+	const [open, setOpen] = useState(false);
+	const toggleOpen = () => {
+		setOpen(!open);
+	};
+	const ReactSingleData = useMemo(() => {
+		return ReactMockData.find((value) => String(value.link) === params.id);
+	}, [params.id]);
 
-  return (
-    <div className=" min-h-screen flex  justify-center text-white  ">
-      {ReactSingleData ? (
-        <div className="flex flex-col gap-10">
-          <div className="w-full  p-4  rounded-lg shadow-lg text-gray-300">
-            {ReactSingleData.component}
-          </div>
-          {ReactSingleData.data && <Code data={ReactSingleData.data} open={open} toggleOpen={toggleOpen} />}
-        </div>
-      ) : (
-        <div className="text-center">
-          <h2 className="text-xl font-semibold">Data Not Found</h2>
-          <p className="text-gray-400">The requested item does not exist.</p>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className=' min-h-screen flex  justify-center text-white  '>
+			{ReactSingleData ? (
+				<div className='flex flex-col gap-10'>
+					<div className='w-full  p-4  rounded-lg shadow-lg text-gray-300'>
+						{ReactSingleData.component}
+					</div>
+					{ReactSingleData.data && (
+						<Code
+							data={ReactSingleData.data}
+							open={open}
+							toggleOpen={toggleOpen}
+						/>
+					)}
+				</div>
+			) : (
+				<div className='text-center'>
+					<h2 className='text-xl font-semibold'>Data Not Found</h2>
+					<p className='text-gray-400'>The requested item does not exist.</p>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default ReactId;
